3d015b37ab25ec8f4957312404b84601ee9bec6e
sventura102 <sventura102@gmail.com>
2021-02-22 19:24:01 -0800

    .

--- js/alarm.js
+++ js/alarm.js
@@ -4,16 +4,13 @@ function Alarm() {
     let month = now.getMonth();
     let dayOfMonth = now.getDate();
     let dayOfWeek = now.getDay();
-    let toDo;
+    
 
-    //Processing - Use If/Else and Logic to determine whether to sleep or to wake up.
+    //Processing & Output- Use If/Else and Logic to determine whether to sleep or to wake up.
     if (dayOfMonth == 1 && month == 0 || dayOfMonth == 4 && month == 6 || dayOfMonth == 25 && month == 11 || dayOfMonth == 25 && month == 11 || dayOfWeek == 0 || dayOfWeek == 6) {
-        toDo = "sleep in."
+        document.getElementById('output').innerHTML = `For today, you can sleep in.`
     }
     else {
-        toDo = "wake up!"
-    };
-
-    //Output = Tell the user what they should do that day.
-    document.getElementById('output').innerHTML = `For today, you will ${toDo}.`;
+        document.getElementById('output').innerHTML = `You have to wake up today!`
+    }
 }
